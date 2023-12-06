d743291a6c0be91067d56883483b4376189ac6f7
Filip C <cermafil@gmail.com>
2023-12-06 11:23:59 +0100

    1/3

---
+++ ClassPlayground/ClassPlayground/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+/*   
+ * 1) Vytvoř třídu Rectangle, kterou budeme reprezentovat obecný obdélník
+ * Přidej třídě Rectangle dvě proměnné - width a height(datový typ nechám na tobě)
+ *    Přidej třídě Rectangle tři funkce - CalculateArea, která spočítá obsah plochy obdélníka
+ *                                      - CalculateAspectRatio, která spočítá poměr stran.Využij spočítaný poměr k určení toho, jestli je obdélník široký, vysoký, nebo je to čtverec
+ *                                      - ContainsPoint, která jako vstupní parametr přijme souřadnice x, y nějakého bodu a určí, jestli se daný bod nachází v obdélníku, nebo ne,
+ *    a podle toho vrátí true/false
+ *    Přidej třídě Rectangle konstruktor, který bude přijímat dva parametry - šířku a výšku, a při jeho zavolání je správně nastaví
+ *
+ * 1) BONUS - Až vytvoříš Rectangle, zkus vytvořit obdobnou třídu se stejnou funkcionalitou pro kruh nebo třeba trojúhelník. 
+ */
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        public double width, height;
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double CalculateArea()
+        {
+            double area = width * height;
+            return area;
+        }
+        public string CalculateAspectRatio()
+        {
+            double width2 = width;
+            double height2 = height;
+            string verdict = "";
+            if (width % height == 0)
+            {
+                width2 = width / height;
+                height2 = height / height;
+            }
+            
+            else if (height % width == 0)
+            {
+
+                height2 = height / width;
+                width2 = width / width;
+            }   
+            string ratio = $"{width2}:{height2}";
+            if (width > height)
+            {
+                verdict = "široký ";
+            }
+            else if (width < height)
+            {
+                verdict = "vyskoý ";
+            }
+            else
+            {
+                verdict = "čtverec ";
+            }
+            verdict = verdict + "a poměr je: " + ratio;
+            return verdict;
+        }
+        public bool ContainsPoint(double x, double y)
+        {
+            bool verdict = false;
+            if (x <= width && y <= height)
+            {
+                verdict = true;
+            }    
+            return verdict;
+        }
+    }
+}
---
+++ ClassPlayground/ClassPlayground/Class2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+/*  2) Vytvoř třídu BankAccount, kterou budeme reprezentovat bankovní účet
+ * Přidej třídě BankAccount čtyři proměnné - accountNumber jako číslo účtu
+ *                                            - holderName jako jméno osoby, které účet patří
+ *                                            - currency jako měna, ve které je účet vedený
+ *                                            - balance jako zůstatek na účtu
+ * Přidej třídě BankAccount čtyři funkce - Deposit, která jako vstupní parametr přijme množství peněz a vloží je na účet
+ *                                          - Withdraw, která jako vstupní parametr přijme množství peněz a z účtu "vybere" peníze, tedy sníží zůstatek a navrátí požadované množství
+ * Pokud na účtu není dostatek peněz, uživatele upozorní a vrátí nulu.
+ *                                          - Transfer, která jako vstupní parametry přijme množství peněz a číslo účtu, na které se budou peníze posílat, a převede peníze
+ * z jednoho účtu na druhý(opět pouze pokud je na účtu, ze kterého převod jde, dostatek peněz)
+ * Přidej třídě BankAccount konstruktor, který bude přijímat dva parametry - jméno majitele účtu a měnu, ve které bude účet vedený
+ *                                                                            - Při jeho zavolání nastav jméno a měnu podle vstupních parametrů, accountNumber nastav jako náhodně
+ * vygenerované číslo velké alespoň 100 000 000 a menší, než 10 000 000 000 a balance nastav na nulu
+ */
+    internal class BankAccount
+    {
+        public int accountNumber;
+        public string HolderName;
+        public string currency;
+        public int balance;
+        public BankAccount(int accountNumber, string currency, string HolderName, int balance)
+        {
+            this.accountNumber = accountNumber;
+            this.HolderName = HolderName;
+            this.currency = currency;
+            this.balance = balance;
+        }
+        public BankAccount() 
+        {
+            
+        }
+        public int Deposit(int amount)
+        {
+            return balance+ amount;
+        }
+        public int withdraw(int amount)
+        {
+            if (balance < amount)
+            {
+                Console.WriteLine($"jsi broked a vybral jsi jen {balance}");
+                return 0;
+            }
+            return balance;
+        }
+        public int Transfer(int amount, int number)
+        {
+
+            BankAccount acc2 = new BankAccount();
+            acc2.balance = acc2.balance + amount;
+            return balance; 
+        }
+    }
+}
